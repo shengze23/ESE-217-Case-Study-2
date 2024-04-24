0f32744788c3c4f3abb421d10efb033b0a0be2c9
csace22003 <60158878+csace22003@users.noreply.github.com>
2024-04-24 16:17:50 -0500

    Update Part2.m

--- Part2.m
+++ Part2.m
@@ -47,8 +47,8 @@ noisyf_map = sortrows(noisyf_map, 'descend');   % key-value pairings preserved
 % define circuit constants and initial conditions
 R2 = 1.6;       % Resistance in Ohms for R2 -> lower impedence on first circuit (R4 = R2 * 10)
 R4 = 16;        % Resistance in Ohms for R4 **CONST**
-C1 = 8.289e-5;  % Capacitance in Farads for C1 -> C1 from cutoff frequency eqn: f_c = 120 Hz (sufficiently above 60 Hz grid)
-C3 = 8.289e-6;  % Capacitance in Farads for C3 -> higher impedence on second circuit (C3 = C1 / 10)
+C1 = 9.947e-5;  % Capacitance in Farads for C1 -> C1 from cutoff frequency eqn: f_c = 100 Hz (sufficiently above 60 Hz grid)
+C3 = 9.947e-6;  % Capacitance in Farads for C3 -> higher impedence on second circuit (C3 = C1 / 10)
 V0 = [0; 0];    % Initial voltages across C1 and C3
 % note: cutoff freq is f_c = 1/(2pi(rc))
 
@@ -92,7 +92,6 @@ playSound(V_out, Fs);
 % Define changed circuit elements
 R1 = R2;        % Resistance in Ohms for R1 -> low impedence on first stage (low-pass)
 C2 = 3.094e-5;  % Capacitance in Farads for C2 -> low-pass stage: cutoff freq is 3214.89 Hz*
-C3 = C1;        % Capacitance in Farads for C3 -> high-pass stage: same as above circuit
 
 % * from band-pass centre frequency formula: f = sqrt(f_c_high * f_c_low)
 %   high-pass stage cutoff freq is same as before at 100 Hz
@@ -127,17 +126,61 @@ grid on;
 
 playSound(V_out2, Fs);
 
+%% Band-pass filter (similar config to fig. 4) but with 2nd Order High-Pass Input
+% This circuit is equivalent to cascading a 2nd order high-pass filter
+% described in fig. 3 with ther first order band-pass filter described in
+% fig. 4
+
+% Our target isolating frequency ranges are the same here so no circuit
+% parameters are changed
+
+[t3, V3] = ode45(@(t, V3) cascadedRCODE_B(t, V_out, V2, R1, R4, C2, C3, round(t / T) + 1), tspan, V0);
+V_out3 = V3(:,1) - V3(:,2);
+
+% Fourier analysis   
+V_out3f = abs(fft(V_out3));
+
+% figure 8: v_out3 fft
+figure;
+hold on;
+plot(X_s, V_out3f);
+hold off;
+
+xlabel("Time (s)");
+ylabel("fft output")
+grid on;
+
+% figure 9: v_out3
+figure;
+plot(t3, V_out3);
+
+xlabel("Time (s)");
+ylabel("Signal Strength (V)");
+grid on;
+
+playSound(V_out3, Fs);
+
+
 %% Results:
 % We would prefer the 2nd order high-pass circuit over the band-pass
 % circuit in this case as though the band-pass circuit is theoretically
 % more versatile in that there is an upper and lower cutoff frequency, the
-% single order setup in this case introduces far more disturbance. As such,
-% even though the 2nd order high-pass circuit does not remove as much of
-% the hiss it does a far better job at removing the 60 Hz hum. In addition,
-% as shown in the fft plots, the majority of the upper frequency
-% disturbances are not audible (with peaks at around 44040 Hz, the upper
-% limit of human hearing is ~ 15-17 kHz) so the fact that high frequency 
-% filtering is absent is less of an issue.  
+% single order setup in this case likely does a worse job at filtering out 
+% unwanted frequencies. Furthermore, while both circuits do a good job at
+% filtering out the 60 Hz hum, the high-pass circuit actually does a better
+% job at filtering out the hissing. This could suggest that the hiss is
+% made up of a significant amount of lower frequency elements not covered
+% by the low-pass portion of the band-pass filter, thus diminishing the
+% usefulness of the band-pass filter's extra functionality. Finally, we
+% also experimented on running the 2nd order high-pass circuit's output
+% through the first order band-pass circuit. Though this method seems to
+% have gotten rid of the hissing present in the other two circuit's
+% outputs, the signal making up the music is also heavily diminished and is
+% quite hard to make out as its signal is now quite close in strength to
+% the reduced hum. Furthermore, this 'cascaded' circuit seems to introduce
+% a high pitched noise in lieu of the hissing as well so the audio signal
+% outputted here is of lower quality by most metrics compared to the 2nd
+% order high-pass circuit by itself.
 
 %% DEs:
 function dVdt = cascadedRCODE_A(~, Vin, V, R2, R4, C1, C3, counter)
